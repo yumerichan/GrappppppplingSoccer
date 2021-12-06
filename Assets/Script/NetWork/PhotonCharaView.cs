4100f2385fc1405f9780d1e32029e87cdc971b7d
Kamichosu <1909408@s.asojuku.ac.jp>
2021-12-06 15:37:29 +0900

    aaa

---
+++ Assets/Script/NetWork/PhotonCharaView.cs
@@ -0,0 +1,50 @@
+using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhotonCharaView : MonoBehaviour, IPunObservable
+{
+    private PhotonView photonView;
+
+    public string _text;
+    public int _aaa;
+
+    public string Text
+    {
+        get { return _text; }
+        set { _text = value; RequestOwner(); }
+    }
+
+    void Awake()
+    {
+        this.photonView = GetComponent<PhotonView>();
+    }
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        // オーナーの場合
+        if (stream.IsWriting)
+        {
+            stream.SendNext(this._text);
+            stream.SendNext(this._aaa);
+        }
+        // オーナー以外の場合
+        else
+        {
+            this._text = (string)stream.ReceiveNext();
+            this._aaa = (int)stream.ReceiveNext();
+        }
+    }
+
+    private void RequestOwner()
+    {
+        if (this.photonView.IsMine == false)
+        {
+            if (this.photonView.OwnershipTransfer != OwnershipOption.Request)
+                Debug.LogError("OwnershipTransferをRequestに変更してください。");
+            else
+                this.photonView.RequestOwnership();
+        }
+    }
+}
