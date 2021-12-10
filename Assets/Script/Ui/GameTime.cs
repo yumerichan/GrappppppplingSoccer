034dedc99bc2fde663eca3cd92bdc249609ff6d5
Kamichosu <81273234+Kamichosu@users.noreply.github.com>
2021-12-10 10:10:27 +0900

    aa

--- Assets/Script/Ui/GameTime.cs
+++ Assets/Script/Ui/GameTime.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using Photon.Pun;
+using UnityEngine.SceneManagement;
 
 public class GameTime : MonoBehaviour
 {
@@ -71,6 +72,19 @@ public class GameTime : MonoBehaviour
 
         //  ŽžŠÔ‚ð•\Ž¦
         gameTimeText_.text = string.Format("{0:00}:{1:00}", minutes, second);
+
+        //=========================================================================
+        if (curTime_ <= 0f)
+        {
+            GameObject.Find("CharaScoreManager").GetComponent<CharaScoreManager>().LetsGoResult();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha0))
+        {
+
+            curTime_ = 0f;
+        }
+        //=========================================================================
     }
 
 
