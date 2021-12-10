2c842bd0e8eab6bf9ceb94f300ccc0ee1ff64349
1909411asoKN <80931930+1909411asoKN@users.noreply.github.com>
2021-12-10 12:40:03 +0900

    rwtwerwerwer

---
+++ Assets/Script/Ui/MenuUI/MenuUi.cs
@@ -0,0 +1,107 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+
+public class MenuUi : MonoBehaviourPunCallbacks
+{
+    public Image[] _menuUi;
+
+    public bool _isOpenMenu { get; set; }
+
+    //  メニュー画面タイプ
+    public enum MenuType
+    {
+        MENU_TYPE_OPTION,       //オプション
+        MENU_TYPE_SETTING,      //設定
+        MENU_TYPE_CAM_SENSI,    //カメラ感度
+        MENU_TYPE_VALUME,       //音量
+        MENU_TYPE_OPERETION,    //操作設定
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _isOpenMenu = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!photonView.IsMine)
+            return;
+
+        //  スタートボタンで開閉
+        if (Input.GetButtonDown("Option"))
+        {
+            //  開いていなかったらメニュー開く
+            if (!_isOpenMenu)
+            {
+                OpenMenu();
+            }
+            //  開かれていたら閉じる
+            else if(_isOpenMenu)
+            {
+                DeleteMenu();
+            }
+        }
+    }
+
+    //  メニュー画面開く
+    public void OpenMenu()
+    {
+        _menuUi[(int)MenuType.MENU_TYPE_OPTION].gameObject.SetActive(true);
+        _isOpenMenu = true;
+    }
+
+    //  メニュー画面削除
+    public void DeleteMenu()
+    {
+        foreach (Image image in _menuUi)
+        {
+            image.gameObject.SetActive(false);
+            _isOpenMenu = false;
+        }
+    }
+
+    //  アクティブセット
+    public void SetMenuActive(MenuType type, bool is_active)
+    {
+        _menuUi[(int)type].gameObject.SetActive(is_active);
+    }
+
+    //  設定画面開く
+    public void OnSetting()
+    {
+        //  メニュー画面を閉じて設定画面を開く
+        SetMenuActive(MenuType.MENU_TYPE_OPTION, false);
+        SetMenuActive(MenuType.MENU_TYPE_SETTING, true);
+
+    }
+
+    //  音量
+    public void OnValume()
+    {
+        //  設定画面を閉じて音量設定画面を開く
+        SetMenuActive(MenuType.MENU_TYPE_SETTING, false);
+        SetMenuActive(MenuType.MENU_TYPE_VALUME, true);
+    }
+
+    //  カメラ感度
+    public void OnCamSensi()
+    {
+        //  設定画面を閉じてカメラ設定画面を開く
+        SetMenuActive(MenuType.MENU_TYPE_SETTING, false);
+        SetMenuActive(MenuType.MENU_TYPE_CAM_SENSI, true);
+    }
+
+    //  操作説明画面開く
+    public void OnOperetion()
+    {
+        //  メニュー画面を閉じて設定画面を開く
+        SetMenuActive(MenuType.MENU_TYPE_OPTION, false);
+        SetMenuActive(MenuType.MENU_TYPE_OPERETION, true);
+    }
+}
