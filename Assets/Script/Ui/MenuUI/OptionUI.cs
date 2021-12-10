458023cc71320f9624117eb245c757af1c2ba4c1
1909411asoKN <80931930+1909411asoKN@users.noreply.github.com>
2021-12-10 11:44:59 +0900

    hukkuobject

---
+++ Assets/Script/Ui/MenuUI/OptionUI.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+
+public class OptionUI : MonoBehaviour
+{
+    public Image[] _opitonUi { get; set; }
+
+    public bool _isOpenOption { get; set; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _isOpenOption = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OpenMenu()
+    {
+
+    }
+
+    public void DeleteMenu()
+    {
+
+    }
+
+    public void SetMenuActive()
+    {
+
+    }
+}
