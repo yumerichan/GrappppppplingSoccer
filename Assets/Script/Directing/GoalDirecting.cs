eee08cab317cd7690f84cf274a86f4c6aea7ceb2
1909411asoKN <80931930+1909411asoKN@users.noreply.github.com>
2022-01-05 22:45:49 +0900

    asd

--- Assets/Script/Directing/GoalDirecting.cs
+++ Assets/Script/Directing/GoalDirecting.cs
@@ -13,9 +13,16 @@ public class GoalDirecting : MonoBehaviour
     [SerializeField]
     private float _slowTime;    //ゲーム時間がどれだけ遅くなるか
     [SerializeField]
-    private Vector3 _initPos;   //移動前の初期位置
+    private Vector3 _initGoalPos;   //移動前の初期位置
+    [SerializeField]
+    private Vector3 _initTeamPos;  //移動前の初期位置
+    [SerializeField]
+    private Image _goalImage;        //ゴール画像
+    [SerializeField]
+    private Image[] _teamImages;        //ゴールしたチーム画像
 
-    private RectTransform _rect;   //Ui移動用座標
+    private RectTransform _Goalrect;   //Ui移動用座標
+    private RectTransform _Teamrect;   //チームUi移動用座標
 
     private float _dispCount;  //表示時間カウント
 
@@ -27,12 +34,22 @@ public class GoalDirecting : MonoBehaviour
     }
     private Phase _phase;
 
+    private enum GoalTeam
+    {
+        TeamNone = -1,
+
+        Red,
+        Blue,
+    }
+    private GoalTeam _goalTeam;
+
     // Start is called before the first frame update
     void Start()
     {
-        _rect = GetComponent<RectTransform>();
+        _Goalrect = _goalImage.GetComponent<RectTransform>();
         _phase = Phase.Idle;
         _dispCount = 0.0f;
+        _goalTeam = GoalTeam.TeamNone;
     }
 
     // Update is called once per frame
@@ -51,20 +68,27 @@ public class GoalDirecting : MonoBehaviour
             //  移動
             case Phase.Move: 
                 {
-                    //  Uiの座標を参照
-                    Vector3 pos = _rect.localPosition;
+                    //  ゴールUI移動
+                    Vector3 pos = _Goalrect.localPosition;
                     pos.x -= _moveSpeed * Time.unscaledDeltaTime * 15;
 
+                    //  チームUI移動
+                    Vector3 team_pos = _Teamrect.localPosition;
+                    pos.x += _moveSpeed * Time.unscaledDeltaTime * 15;
+
+
+                    //  座標を反映
+                    _Goalrect.localPosition = pos;
+                    _Teamrect.localPosition = team_pos;
+
                     //  移動終わり
-                    if(pos.x < 0)
+                    if (pos.x < 0)
                     {
                         //  表示フェーズへ
                         pos.x = 0.0f;
                         _phase = Phase.Display;
                     }
 
-                    //  座標を反映
-                    _rect.localPosition = pos;
                 }
                 break;
 
@@ -82,8 +106,10 @@ public class GoalDirecting : MonoBehaviour
         }
     }
 
+    //  =======ゴールが入った時の演出を呼ぶ==========
+
     [PunRPC]
-    //  ゴールが入った時の演出を呼ぶ
+   //   赤ゴール
     public void RequestGoalDirecting()
     {
         //  ゲームをスローモーションにする
@@ -91,6 +117,28 @@ public class GoalDirecting : MonoBehaviour
 
         //  移動フェーズに移行
         _phase = Phase.Move;
+
+        //  赤チーム
+        _goalTeam = GoalTeam.Red;
+        _Teamrect = _teamImages[(int)_goalTeam].GetComponent<RectTransform>();
+
+        //  リスタートのフラグを折っておく
+        GameObject.FindGameObjectWithTag("playManager").GetComponent<PlayScene>()._isGoalDirecting = false;
+    }
+
+    [PunRPC]
+    //  青ゴール
+    public void RequestBlueGoalDirecting()
+    {
+        //  ゲームをスローモーションにする
+        Time.timeScale = _slowTime;
+
+        //  移動フェーズに移行
+        _phase = Phase.Move;
+
+        //  青チーム設定
+        _goalTeam = GoalTeam.Blue;
+        _Teamrect = _teamImages[(int)_goalTeam].GetComponent<RectTransform>();
 
         //  リスタートのフラグを折っておく
         GameObject.FindGameObjectWithTag("playManager").GetComponent<PlayScene>()._isGoalDirecting = false;
@@ -103,9 +151,9 @@ public class GoalDirecting : MonoBehaviour
         Time.timeScale = 1.0f;
 
         //  Uiを初期位置に設定
-        Vector3 pos = _rect.localPosition;
-        pos = _initPos;
-        _rect.localPosition = pos;
+        Vector3 pos = _Goalrect.localPosition;
+        pos = _initGoalPos;
+        _Goalrect.localPosition = pos;
 
         //  表示時間リセット
         _dispCount = 0.0f;
